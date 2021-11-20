ea92bc931eed311851a399d45c155919fc9f0a6a
= <Emmanuelsumeh@gmail.com>
2021-11-20 13:40:22 +0100

    feat: added new features to smart contract

---
+++ src/utils/constants.js
@@ -0,0 +1,5 @@
+const MPContractAddress = "0x8d135DC6E7A36018541487e1ba03720b8A0F57Ca";
+const cUSDContractAddress = "0x874069Fa1Eb16D44d622F2e0Ca25eeA172369bC1";
+const ERC20_DECIMALS = 18;
+
+export { MPContractAddress, cUSDContractAddress, ERC20_DECIMALS };
